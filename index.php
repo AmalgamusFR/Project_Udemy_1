1006093aa5f0a42c815a1c9993b82e11a690d633
AmalgamusFR <amalgamus@wanadoo.fr>
2020-04-23 20:42:40 -1000

    Add index.php to Git

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+ $var1="index.html";
+
+
+$var2="<html>
+<head>
+    <body></br>Hello there !!
+    <form action='http://localhost/lessons/index2.php' method = 'POST'>
+    <input type='text' name='name'></input> </br>
+    <input type='submit' value='Submit'>
+    </form>
+    </body>
+</head>
+</html>";
+
+echo $var1;
+echo $var2;
+
+
+
+
+?>
